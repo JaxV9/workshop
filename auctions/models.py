89ea840abff37270c5a9538fd7b783eaaa099b7c
LAYAN_Jason <layanj.pro@gmail.com>
2023-05-30 12:08:24 +0200

    feat: Database created

--- auctions/models.py
+++ auctions/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -10,43 +11,34 @@ class Category(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+    
+class State(models.Model):
+    state = models.CharField(max_length=64)
 
-class Listing(models.Model):
+    def __str__(self):
+        return f"{self.state}"
+
+
+class Product(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.URLField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="listings")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="products")
+    image = models.URLField(max_length=200, blank=True)
+    exchange = models.BooleanField(null=True, default=False)
+    state = models.ForeignKey(State, on_delete=models.CASCADE, related_name="products")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    localisation = models.CharField(max_length=64)
     date = models.DateTimeField(auto_now_add=True)
-    statut = models.BooleanField(null=True, default=True)
 
     def __str__(self):
         return f"{self.title} {self.user}"
 
 
-
-class Wishlist(models.Model):
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="wishlist",related_query_name="wishlist")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wishlist", default=None)
-    
-    def __str__(self):
-        return f"{self.listing}"
-
-class Bid(models.Model):
-    bid = models.DecimalField(max_digits=10, decimal_places=2)
-    win = models.BooleanField(null=True, default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
-
-    def __str__(self):
-        return f"{self.bid} {self.user} {self.listing}"
-
-
-class Comments(models.Model):
-    comment = models.TextField()
+class Comment(models.Model):
+    content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="comments",  null=True)
 
     def __str__(self):
-        return f"{self.comment} {self.user} {self.listing}"
+        return f"{self.content} {self.user} {self.product}"
--- auctions/urls.py
+++ auctions/urls.py
@@ -8,11 +8,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("create_listing", views.create_listing, name="create_listing"),
-    path("listing/<int:listing_id>", views.listing, name="listing"),
-    path("listing_register/", views.listing_register, name="listing_register"),
-    path('delete_wishlist/<int:pk>', views.delete_wishlist, name='delete_wishlist'),
-    path('categories', views.categories, name='categories'),
-    path('category/<int:category_id>', views.category, name='category'),
+    path("register", views.register, name="register")
 ]
